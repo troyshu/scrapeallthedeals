272477266a92784602067892c3e2eb4e3e205e82
Troy Shu <tmshu1@gmail.com>
2012-12-05 11:14:32 -0500

    test scrape relevant deal items

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,12 @@
+require 'StaticPagesHelper'
+
 class StaticPagesController < ApplicationController
   def home
   end
 
   def help
   end
+
 
   def scrape
   	#xpath for a living social deal:
@@ -15,7 +18,14 @@ class StaticPagesController < ApplicationController
 	response = page.content
 	doc = Hpricot(response)
 	deals=(doc/"/html/body[@class='external www external-www cities cities-show full-width']/div[@class='container main-content']/div[@class='row']/div[@class='span12 content']/div[@id='cities-content-id']/ul[@class='unstyled cities-items']/li[@class^='ls-item deal ']")
-	
+	deals.each do |deal|
+		name = (deal/"/div[@class='bd']/h1")[0].innerHTML
+		headline = (deal/"/div[@class='bd']/h2")[0].innerHTML
+		price = parsePrice((deal/"/div[@class='bd']/p[@class='meta']/span[@class='price']")[0].innerHTML)
+		location = (deal/"/div[@class='hd']/div[@class='meta']/span[@class='deal-location']")[0].innerHTML
+
+	end
+
 
   end
 end
