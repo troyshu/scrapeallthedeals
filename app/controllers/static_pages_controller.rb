935e85882869f2e0e2318a331c205017fd954fe5
Troy Shu <tmshu1@gmail.com>
2012-12-05 10:48:13 -0500

    figure out hpricot and mechanize scraping

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,15 @@ class StaticPagesController < ApplicationController
   end
 
   def scrape
-  	
+  	#xpath for a living social deal:
+  	#/html/body[@class='external www external-www cities cities-show full-width']/div[@class='container main-content']/div[@class='row']/div[@class='span12 content']/div[@id='cities-content-id']/ul[@class='unstyled cities-items']/li[@class='ls-item deal']
+
+  	#LIVINGSOCIAL
+	agent = Mechanize.new
+	page = agent.get('http://www.livingsocial.com/cities/4-los-angeles')
+	response = page.content
+	doc = Hpricot(response)
+	deals=(doc/"/html/body[@class='external www external-www cities cities-show full-width']/div[@class='container main-content']/div[@class='row']/div[@class='span12 content']/div[@id='cities-content-id']/ul[@class='unstyled cities-items']/li[@class='ls-item deal ']")
+	
   end
 end
